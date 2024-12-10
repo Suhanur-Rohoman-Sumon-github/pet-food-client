b34701f170d5d72874f29503173c720e4a416ac5
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2024-12-10 14:52:32 +0600

    admin dashboard added

---
+++ src/app/(adminDashboard)/admin/create-products/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full h-screen border border-red-500">
+      <h1>this is create products</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/ui/app-sidebar.tsx
@@ -0,0 +1,19 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <h1>hit </h1>
+      </SidebarHeader>
+      <SidebarContent></SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
